7908a47103e97b1cdeff8041639b320f131c8345
Dhrumil Bhut <dhrumilbhut@gmail.com>
2023-01-27 15:50:10 +0530

    initial commit client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
