5b800ef4a3baeca2257da4840c7531b98ab44896
Dhrumil Bhut <dhrumilbhut@gmail.com>
2023-02-13 08:57:34 +0530

    removed: Featured list

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -10,7 +10,7 @@ const FeaturedProperties = () => {
         "Loading"
       ) : (
         <>
-          {data.map((item) => (
+          {data?.map((item) => (
             <div className="fpItem" key={item._id}>
               <img src={item.photos[0]} alt="" className="fpImg" />
               <span className="fpName">{item.name}</span>
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import Featured from "../../components/featured/Featured";
-import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+// import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
@@ -17,7 +17,7 @@ const Home = () => {
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
-        <FeaturedProperties />
+        {/* <FeaturedProperties /> */}
         <MailList />
         <Footer />
       </div>
